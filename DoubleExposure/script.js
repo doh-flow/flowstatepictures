d3aa0065f858df544775cc8e5a920ac4d22e91c6
doh-flow <andrewdoh2005@gmail.com>
2026-07-15 02:52:28 -0400

    Update script.js

---
+++ DoubleExposure/script.js
@@ -0,0 +1,7 @@
+const MEDIA = "https://pub-afc509d8cc7d4e5f870756f5f37512c1.r2.dev/";
+
+document.getElementById('logo').src = MEDIA + "gallery/FlowstateLogo.png";
+document.getElementById('source').src = MEDIA + "main/hero.mp4";
+document.getElementById("member1").src = MEDIA + "main/m1.jpg";
+document.getElementById("member2").src = MEDIA + "main/m2.jpg";
+document.getElementById("member3").src = MEDIA + "main/m3.jpg";
