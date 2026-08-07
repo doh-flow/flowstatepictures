f9c207f611a0e03e90bede88c1b2170331f6447d
doh-flow <andrewdoh2005@gmail.com>
2026-08-06 23:38:33 -0400

    Update script.js

--- main2/script.js
+++ main2/script.js
@@ -1,3 +1,7 @@
+const MEDIA = "https://pub-afc509d8cc7d4e5f870756f5f37512c1.r2.dev/";
+
+document.getElementById('logo').src = MEDIA + "main/FlowstateLogo.png";
+
 const StillDesc = [
     "",
     "",
